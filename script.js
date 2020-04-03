c440ad2a7dad0ca3bbe4b6691f6d72e394ba797e
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-04-03 14:49:05 -0400

    Removed one for loop and replaced with event listener & button

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 
 var initials;
-var score = 0;
+var score = 0;//correct
+var questionNumber = 0;// Keeps track of questions asked
 var highscore;
 var timerCountdown;
 
-// Array of questions and answers
-
+// Array of questions, answers, and correct answers
 var questions = [
-{ q: "Inside which HTML element do we put the JavaScript?", options: {option1: "<script>", option2: "<javascript>", option3: "<js>"}, answer: "<script>"},
-{ q: "Second Question.", options: {option1: "a", option2: "b", option3: "c"}, answer: "b"},
-{ q: "Third Question.", options: {option1: "a", option2: "b", option3: "c"}, answer: "c"}
+{ q: "Inside which HTML element do we put the JavaScript?", options: {option1: "script", option2: "javascript", option3: "js"}, answer: "option1"},
+{ q: "Second Question.", options: {option1: "a", option2: "b", option3: "c"}, answer: "option2"},
+{ q: "Third Question.", options: {option1: "a", option2: "b", option3: "c"}, answer: "option3"}
 ];
 
 // When the Start button is clicked page loads: timer starts and question starts
@@ -24,87 +24,58 @@ function startQuiz(){
     if (startTime <= 0)
         clearInterval(timer);
     }, 1000);
-  //When I answer a question I am presented with another question                    
-  for (var i = 0; i < questions.length; i++){
+  
+  // Declare variables for <h2> & <label> element tags
+  var headingElement = document.getElementById("heading");
+  var label1Element = document.getElementById("label1");
+  var label2Element = document.getElementById("label2");
+  var label3Element = document.getElementById("label3");
 
-    // sets heading to question
-    var headingElement = document.getElementById("heading");
-    var question = questions[i].q;
-    headingElement.textContent = question;
-    // sets label id for each question to a variable
-    var label1Element = document.getElementById("label1");
-    var label2Element = document.getElementById("label2");
-    var label3Element = document.getElementById("label3");
+      
+  // sets heading to question   
+    var question = questions[questionNumber].q; //questions[0].q
+    headingElement.textContent = question; //shows question in <h2>
+    
     // adds input radio buttons to each label and adds answer options to each 
-    var option1Btn = questions[i].options.option1;
-    label1Element.innerHTML = "<input id=\"label1\" type=\"radio\" name=\"optradio\"> " + option1Btn;
-    console.log(option1Btn);
-    var option2Btn = questions[i].options.option2;
-    label2Element.innerHTML = "<input id=\"label2\" type=\"radio\" name=\"optradio\"> " + option2Btn;
-    console.log(option2Btn);
-    var option3Btn = questions[i].options.option3;
-    label3Element.innerHTML = "<input id=\"label3\" type=\"radio\" name=\"optradio\"> " + option3Btn;
-    console.log(option3Btn);
+    var option1Btn = questions[questionNumber].options.option1;
+    label1Element.innerHTML = "<input id='label1' type='radio' name='optradio' value='option1'> " + option1Btn;    
+    var option2Btn = questions[questionNumber].options.option2;
+    label2Element.innerHTML = "<input id='label2' type='radio' name='optradio' value='option2'> " + option2Btn;    
+    var option3Btn = questions[questionNumber].options.option3;
+    label3Element.innerHTML = "<input id='label3' type='radio' name='optradio' value='option3'> " + option3Btn;
 
-    // save each radio button to each radio button variable
-    var radio1Btn = document.getElementById("radio1");
-    var radio2Btn = document.getElementById("radio2");
-    var radio3Btn = document.getElementById("radio3");
-    
-
-    radio1Btn.addEventListener("click", function(){
-        //console.log(option1Btn);
-        var answers = questions[i].answer;
-        if(option1Btn === answers){
-            console.log("match");
-            document.getElementById("message").textContent = "Correct!";
-            
-            
-            
-        } else {
-            console.log("no match");
-            document.getElementById("message").textContent = "Wrong Answer!";
-        }
-
-    });
-
-    radio2Btn.addEventListener("click", function(){
-        //console.log(option2Btn);
-        var answers = questions[i].answer;
-        if(option2Btn === answers){
-            console.log("match");
-            document.getElementById("message").textContent = "Correct!";
-            
-        } else {
-            console.log("no match");
-            document.getElementById("message").textContent = "Wrong Answer!";
-        }
-        
-    });
-
-    radio3Btn.addEventListener("click", function(){
-        //console.log(option3Btn);
-        var answers = questions[i].answer;
-        if(option3Btn === answers){
-            console.log("match");
-            document.getElementById("message").textContent = "Correct!";
-            
-        } else {
-            console.log("no match");
-            document.getElementById("message").textContent = "Wrong Answer!";
-        }
-    });
-
-
-    
-
-  }
 }
 
+var submitBtnElement = document.getElementById("submit");
+var answer = questions[questionNumber].answer;
+
+    // When button clicked check which button pressed
+    submitBtnElement.addEventListener("click", function(event){
+        event.preventDefault();
+        var answerSelected;
+        var options = document.getElementsByName('optradio');//collection of obj/array; type name optradio
+        // loop runs through each option answer
+        for (var i = 0; i < options.length; i++){ // i < 3
+            if (options[i].checked){                
+                answerSelected = options[i].value;//save checked value in variable
+                //console.log(answerSelected);
+            }
+        }
+        var answer = questions[questionNumber].answer;
+        if (answerSelected === answer){
+            document.getElementById("message").textContent = "Correct!";
+            score+=10;
+        } else {
+            document.getElementById("message").textContent = "Incorrect!";
+        }
+        questionNumber++;
+        startQuiz();  
+
+    });
 
 
 
-//startQuiz();
+
 
 
 
