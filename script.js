290554d40ec7eea8064f3f4e91d218421dbaf525
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-04-03 23:08:07 -0400

    Added increaseScore & game over function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 var initials;
-var score = 0;//correct
+
 var questionNumber = 0;// Keeps track of questions asked
 var highscore;
 var timerCountdown;
@@ -14,33 +14,33 @@ var questions = [
 ];
 
 //start timer 
-var startTime = 60;
+var startTime = 30;
 var timer = setInterval(function() {
 startTime--;
 var timerSpan = document.getElementById("timer");
 timerSpan.textContent = startTime;
-if (startTime <= 0)
+if (startTime <= 0){
     clearInterval(timer);
+}   
 }, 1000);
 
 // When the Start button is clicked page loads: timer starts and question starts
 function startQuiz(){
-
-  
-  // Declare variables for <h2> & <label> element tags
-  var headingElement = document.getElementById("heading");
-  var label1Element = document.getElementById("label1");
-  var label2Element = document.getElementById("label2");
-  var label3Element = document.getElementById("label3");
-
+    
+    
+    // Declare variables for <h2> & <label> element tags
+    var headingElement = document.getElementById("heading");
+    var label1Element = document.getElementById("label1");
+    var label2Element = document.getElementById("label2");
+    var label3Element = document.getElementById("label3");
       
   // sets heading to question   
-    var question = questions[questionNumber].q; //questions[0].q
+    var question = questions[questionNumber].q;
     headingElement.textContent = question; //shows question in <h2>
     
     // adds input radio buttons to each label and adds answer options to each 
     var option1Btn = questions[questionNumber].options.option1;
-    label1Element.innerHTML = "<input id='label1' type='radio' name='optradio' value='option1'> " + option1Btn;    
+    label1Element.innerHTML = "<input id='label1' type='radio' name='optradio' value='option1' checked> " + option1Btn;    
     var option2Btn = questions[questionNumber].options.option2;
     label2Element.innerHTML = "<input id='label2' type='radio' name='optradio' value='option2'> " + option2Btn;    
     var option3Btn = questions[questionNumber].options.option3;
@@ -48,44 +48,66 @@ function startQuiz(){
 
 }
 
+
 var submitBtnElement = document.getElementById("submit");
 var answer = questions[questionNumber].answer;
 
-    // When button clicked check which button pressed
-    submitBtnElement.addEventListener("click", function(event){
-        event.preventDefault();
-        var answerSelected;
-        var options = document.getElementsByName('optradio');//collection of obj/array; type name optradio
-        // loop runs through each option answer
-        for (var i = 0; i < options.length; i++){ // i < 3
-            if (options[i].checked){                
-                answerSelected = options[i].value;//save checked value in variable
-                //console.log(answerSelected);
-            }
+// When button clicked check which button pressed
+submitBtnElement.addEventListener("click", function(event){
+    event.preventDefault();
+    var answerSelected;
+    var options = document.getElementsByName('optradio');//collection of obj/array; type name optradio
+    // loop runs through each option answer
+    for (var i = 0; i < options.length; i++){ // i < 3
+        if (options[i].checked){                
+            answerSelected = options[i].value;//save checked value in variable
+            //console.log(answerSelected);
         }
-        var answer = questions[questionNumber].answer;
-        if (answerSelected === answer){
-            document.getElementById("message").textContent = "Correct!";
-            score+=10;
-        } else {
-            document.getElementById("message").textContent = "Incorrect!";
-            startTime-=10; // decrease time by 10 seconds            
-        }
-        questionNumber++;
-        startQuiz();  
+    } // if answered selected equals answer of current question 
+    var answer = questions[questionNumber].answer;
+    if (answerSelected === answer){
+        document.getElementById("message").textContent = "Correct!";
+        increaseScore();
+        
+        
+    } else {
+        document.getElementById("message").textContent = "Incorrect!";
+        startTime-=10; // decrease time by 10 seconds            
+    }
+    questionNumber++;
 
-    });
+    gameOver();
+    
+    //Go to next question
+    startQuiz();  
+
+});
 
 
+var score = localStorage.getItem("count", 0);//correct
 
-
-
-
-
+function increaseScore(){
+    score++;    
+    localStorage.setItem("count", score);
+     
+}
         
 //When all questions are answered OR timer reaches 0
     //game is over
 
+
+    
+function gameOver(){  
+    var currentScore;  
+    if ((startTime <= 0) || (questionNumber >= questions.length)){
+        window.location.href = "game-over.html";
+        var scoreSpan = document.getElementById("score");        
+        currentScore = localStorage.getItem("count");        
+        scoreSpan.textContent = currentScore;      
+    
+     }
+
+}
 
 //When the game is over
     //then I can save my initials and score
